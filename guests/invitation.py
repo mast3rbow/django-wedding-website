394e001bc73ef6b35adeb8794c254a1a2ad6c59c
Bryton Wishart <bryton@curedcompliance.com>
2023-09-20 10:04:19 +1000

    Update invitation.py

--- guests/invitation.py
+++ guests/invitation.py
@@ -67,7 +67,7 @@ def send_invitation_email(party, test_only=False, recipients=None):
 def send_all_invitations(test_only, mark_as_sent):
     to_send_to = Party.in_default_order().filter(is_invited=True).exclude(is_attending=False)
     for party in to_send_to:
-        send_invitation_email(party, test_only=test_only, recipients=None)
+        send_invitation_email(party, test_only=False, recipients=None)
         if mark_as_sent:
             party.invitation_sent = datetime.now()
             party.save()
